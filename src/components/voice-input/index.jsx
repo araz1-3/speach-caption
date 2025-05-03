91eabc06bf89d34a159bc8a9173643347556ee22
Alireza Mahmoudi <alireza.m@arianalabs.io>
2025-05-04 00:36:19 +0330

    second commit

--- src/components/voice-input/index.jsx
+++ src/components/voice-input/index.jsx
@@ -1,78 +1,3 @@
-// import React, { useState } from "react";
-// import useSpeachToText from "../../hooks/useSpeachToText";
-
-// function VoiceInput() {
-//   const [textInput, setTextInput] = useState("");
-//   const {isListening, transcript, startListening, stopListening} =
-//     useSpeachToText({ continuous: true });
-
-//   const startStopListening = () => {
-//     isListening ? stopListening() : startListening();
-//   };
-
-//   const stopVoiceInput = () => {
-//     setTextInput(
-//       (prevVal) =>
-//         prevVal +
-//         (transcript.length ? (prevVal.length ? "" : "") + transcript : "")
-//     );
-//     stopListening();
-//   };
-
-//   return (
-//     <div
-//       style={{
-//         display: "block",
-//         margin: "0 auto",
-//         width: "400px",
-//         textAlign: "center",
-//         marginTop: "200px",
-//       }}
-//     >
-//       <button
-//         onClick={startStopListening}
-//         style={{
-//           backgroundColor: isListening ? "#d62d20" : "#008744",
-//           color: "white",
-//           padding: "10px,20px",
-//           border: "none",
-//           borderRadius: "5px",
-//           cursor: "pointer",
-//           transition: "bacckground-color 0.3s ease",
-//         }}
-//       >
-//         {isListening ? " Stop Listening" : "Speak"}
-//       </button>
-//       <textarea
-//         style={{
-//           marginTop: "20px",
-//           width: "100%",
-//           height: "150px",
-//           padding: "10px",
-//           border: "1px solid #ccc",
-//           borderRadius: "5px",
-//           transition: "all 0.3s ease",
-//           resize: "none",
-//           backgroundColor: "#f8f8f8",
-//           color: "#333",
-//         }}
-//         disabled={isListening}
-//         value={
-//           isListening
-//             ? textInput +
-//               (transcript.length
-//                 ? (textInput.length ? " " : "") + transcript
-//                 : "")
-//             : textInput
-//         }
-//         onChange={(e) => setTextInput(e.target.value)}
-//       />
-//     </div>
-//   );
-// }
-
-// export default VoiceInput;
-
 import React, { useState } from "react";
 import useSpeechToText from "../../hooks/useSpeachToText";
 
@@ -80,20 +5,17 @@ function VoiceInput() {
   const [textInput, setTextInput] = useState("");
   const [lang, setLang] = useState("en-US");
 
-  const {
-    isListening,
-    transcript,
-    startListening,
-    stopListening,
-  } = useSpeechToText({ continuous: true, lang });
+  const { isListening, transcript, startListening, stopListening } =
+    useSpeechToText({ continuous: true, lang });
 
   const startStopListening = () => {
     isListening ? stopListening() : startListening();
   };
 
   const stopVoiceInput = () => {
-    setTextInput((prevVal) =>
-      prevVal + (transcript ? (prevVal ? " " : "") + transcript : "")
+    setTextInput(
+      (prevVal) =>
+        prevVal + (transcript ? (prevVal ? " " : "") + transcript : "")
     );
     stopListening();
   };
@@ -159,7 +81,9 @@ function VoiceInput() {
         value={
           isListening
             ? textInput +
-              (transcript.length ? (textInput.length ? " " : "") + transcript : "")
+              (transcript.length
+                ? (textInput.length ? " " : "") + transcript
+                : "")
             : textInput
         }
         onChange={(e) => setTextInput(e.target.value)}
@@ -169,4 +93,3 @@ function VoiceInput() {
 }
 
 export default VoiceInput;
-
